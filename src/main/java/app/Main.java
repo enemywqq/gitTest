ced16fc42c9eceb6754c82476f986b709f97fdbb
realn7 <tolayshapa@gmail.com>
2025-11-07 22:06:24 +1000

    main: add Main and Calculator

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = Calculator.add(5, 3);
+        int product = Calculator.multiply(5, 3);
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Product = " + product);
+    }
+}
